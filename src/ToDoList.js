41aadafe4a26de3bff5bdc11e2b0b2259aff9500
Grzegorz Radomski <rc36@wp.pl>
2018-05-10 10:27:43 +0200

    add input and button and export it to app.js

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-class ToDoList extends React.Component{
-    render(){
-        return(
-            <div className="todos-list">
 
-          <h1>Todos List</h1>
+class ToDoList extends Component {
+    render() {
+        return (
+            <div className="todoList">
+                <h1>Lorem ipsum</h1>
+                <div class="header">
+                    <form>
+                        <input placeholder="enter task">
+                        </input>
+                        <button type="submit">add</button>
+                    </form>
+                </div>
             </div>
         );
 
