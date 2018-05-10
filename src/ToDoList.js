7515ff2ff2ea35548ea28e1e5d16552bfc5aebee
Grzegorz Radomski <rc36@wp.pl>
2018-05-10 05:30:05 +0200

    todo list building

---
+++ src/ToDoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ToDoList extends React.Component{
+    render(){
+        return(
+            <div className="todos-list">
+
+          <h1>Todos List</h1>
+            </div>
+        );
+
+    }
+
+}
+
+
+export default ToDoList;
